c27c217df5d7f7a0e459520c4876fd9d94390b29
Kristjan Lääne <xonproject@gmail.com>
2024-08-13 18:07:07 +0300

    textarea

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+import Warning from './Warning';
+
 export default function Textarea() {
-  return <textarea />;
+  const [text, setText] = useState<string>('');
+  const [showWarning, setShowWarning] = useState<boolean>(false);
+
+  const handleChange = (e: any) => {
+    let newText = e.target.value;
+
+    if (newText.includes('<script>')) {
+      setShowWarning(true);
+      newText = newText.replace('<script>', '');
+    }
+
+    setText(newText);
+  };
+
+  return (
+    <div className='textarea'>
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder='Enter your text...'
+        spellCheck='false'
+      />
+      {showWarning ? <Warning /> : ''}
+    </div>
+  );
 }
