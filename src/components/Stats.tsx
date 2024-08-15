1ff1643a8ac10bb7837c507e78ca09c6901c54e1
Kristjan Lääne <xonproject@gmail.com>
2024-08-15 23:19:45 +0300

    going over limit makes number red

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -29,7 +29,11 @@ type StatProps = {
 function Stat({ label, number }: StatProps) {
   return (
     <section className='stat'>
-      <span className='stat__number'>{number}</span>
+      <span
+        className={`stat__number ${number < 0 ? 'stat__number--limit' : ''}`}
+      >
+        {number}
+      </span>
       <h2 className='second-heading'>{label}</h2>
     </section>
   );
