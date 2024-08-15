2c10ad35e27e8edbe270bf125a07a38a702720f4
Kristjan Lääne <xonproject@gmail.com>
2024-08-15 21:22:44 +0300

    cleanup

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -3,7 +3,6 @@ import Warning from './Warning';
 
 export default function Textarea() {
   const [text, setText] = useState<string>('');
-  const [showWarning, setShowWarning] = useState<boolean>(false);
   const [warningText, setWarningText] = useState<string>('');
 
   const handleChange = (e: any) => {
@@ -11,12 +10,11 @@ export default function Textarea() {
 
     if (newText.includes('<script>')) {
       setWarningText('No script tag allowed!');
-      setShowWarning(true);
+
       newText = newText.replace('<script>', '');
     } else if (newText.includes('@')) {
       setWarningText('No @ symbol allowed!');
 
-      setShowWarning(true);
       newText = newText.replace('@', '');
     }
 
@@ -31,7 +29,7 @@ export default function Textarea() {
         placeholder='Enter your text...'
         spellCheck='false'
       />
-      {showWarning ? <Warning warningText={warningText} /> : ''}
+      <Warning warningText={warningText} />
     </div>
   );
 }
